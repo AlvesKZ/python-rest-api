e53497f8c41c8fa2114176fe239f1883a55d11d3
AlvesKZ <igoralvex01@gmail.com>
2025-06-07 13:08:11 -0300

    feat: criar metodos de listar usuários

--- api_rest/views.py
+++ api_rest/views.py
@@ -5,9 +5,52 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import User, 
+from .models import User
 from .serializers import UserSerializer
 
 import json
 
+@api_view(['GET'])
+def get_users(request):
+    
+    if request.method == 'GET':
+        users = User.objects.all()
+        
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_400)
 
+@api_view(['GET'])
+def get_by_nick(request, nick):
+    
+    try:
+        user = User.objects.get(pk=nick)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def user_manager(request):
+
+    try:
+        if request.GET['user']:
+
+            user_nickname = request.GET['user']
+
+            try:
+                user = User.objects.get(pk=user_nickname)
+            except:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            
+            serializer = UserSerializer(user)
+            return Response(serializer.data) 
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
